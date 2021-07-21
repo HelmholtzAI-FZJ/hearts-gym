77791949ccff1e154bb401d58a65bd03cd43a21b
Michael Osthege <m.osthege@fz-juelich.de>
2021-07-21 23:02:34 +0200

    Solidify logic functions

--- hearts_gym/utils/logic.py
+++ hearts_gym/utils/logic.py
@@ -31,13 +31,18 @@ def gets_trick(card: Card, table_cards: Iterable[Card], cards_by_others: Iterabl
     lead_suit = table_cards[0].suit if len(table_cards) else card.suit
     lead_rank = table_cards[0].rank if len(table_cards) else card.rank
 
-    # Based on the cards already included in the tick...
-    table_cards_that_beat_this = filter_cards_that_get(table_cards, lead_suit, lead_rank)
+    if card.suit != lead_suit:
+        return NEVER
+    # We are qualified based on suit!
+
+    # We have established that our card has the correct suit.
+    # Are there cards on the table that are ranked higher?
+    table_cards_that_beat_this = filter_cards_above(table_cards, card.suit, card.rank)
     if table_cards_that_beat_this:
         return NEVER
 
     # Based on unplayed cards by competitors...
-    alien_cards_that_beat_this = filter_cards_that_get(cards_by_others, lead_suit, lead_rank)
+    alien_cards_that_beat_this = filter_cards_above(cards_by_others, card.suit, card.rank)
     if alien_cards_that_beat_this:
         if len(cards_by_others) < 4:
             # This is the last tick, so that one competitor card that beats us WILL be played.
@@ -54,11 +59,11 @@ def gets_trick(card: Card, table_cards: Iterable[Card], cards_by_others: Iterabl
     return ALWAYS
 
 
-def filter_cards_that_get(cards: Iterable[Card], lead_suit:Optional[int], lead_rank: Optional[int]) -> Tuple[Card]:
-    """Returns all elements from `cards` that would "win" over a certain lead."""
+def filter_cards_above(cards: Iterable[Card], ref_suit:int, ref_rank: int) -> Tuple[Card]:
+    """Returns all elements from `cards` that have the same suit, and a higher rank."""
     return tuple(
         c
         for c in cards
-        if c.suit == lead_suit and c.rank > lead_rank
+        if c.suit == ref_suit and c.rank > ref_rank
     )
 
