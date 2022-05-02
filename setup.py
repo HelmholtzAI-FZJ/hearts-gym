fa8c9b9adedc43eee95a450dcab259fac8f63ad9
janEbert <janpublicebert@posteo.net>
2022-05-02 15:24:30 +0200

    Update Ray version
    Very important due to security issues. We adjust the required Python
    version accordingly to the new wheel builds and EOL of Python 3.6.

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup
 
 setup(
     name='hearts_gym',
-    # RLlib wheels built up to and including 3.8
-    python_requires='>=3.6<3.9',
+    # Ray wheels built up to and including 3.9.
+    python_requires='>=3.7<3.10',
     version='0.0.1',
     install_requires=[
         'gym>=0.18.0',
         'numpy>=1.17',
-        # 1.4.0 and 1.4.1 have forced TensorFlow installation.
-        'ray[default,rllib,tune]>=1.3.0,!=1.4.0,!=1.4.1,<2.0',
+        'ray[default,rllib,tune]==1.12,<2.0',
     ],
 )
